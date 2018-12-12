1399e87724a1937782c4d70605cb2d94f061930e
github username <github email address>
2018-12-12 18:33:33 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
+
+function nestedTarget(){
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n) {
+const rankedList = document.querySelectorAll('.ranked-list li')
+  for(let i = 0; i <rankedList.length; i++ ) {
+  let initial = parseInt(rankedList[i].textContent)
+  rankedList[i].textContent = initial + n
+  }
+}
+
+
+function deepestChild(){
+  return document.querySelector('#grand-node div div div div')
+
+}
